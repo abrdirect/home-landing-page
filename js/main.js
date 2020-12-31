a9c87157546d512c7cce7a1e997e8d8381e70398
Abraham <abrdirect@gmail.com>
2020-12-30 20:50:48 -0800

    Set time interval function for clock

--- js/main.js
+++ js/main.js
@@ -7,17 +7,35 @@ fetch('https://api.quotable.io/random')
         document.querySelector('#author').innerHTML = `${data.author}`;
   })
   .catch(err => console.log(err))
+  
+  
+  function displayTime() {
+        
+        const date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let ampm = '';
+        
+        
+        if (hours > 12) {
+              hours = hours - 12;
+              ampm = 'pm';
+        } else { ampm = 'am' };
+        
+        if (minutes < 10) {
+              minutes = '0' + minutes;
+        };
+        
+        if (seconds < 10) {
+              seconds = '0' + seconds;
+        };
 
-
-
-
-const date = new Date();
-const hour = date.getHours();
-const min = date.getMinutes();
-
-
-function displayTime() {
-      console.log(hour, min);
+      document.querySelector('#hours').innerHTML = `${hours}`;
+      document.querySelector('#minutes').innerHTML = `${minutes}`;
+      document.querySelector('#seconds').innerHTML = `${seconds}`;
+      document.querySelector('#ampm').innerHTML = `${ampm}`;
 }
 
-displayTime();
+
+setInterval(displayTime, 1000);
